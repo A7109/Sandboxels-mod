17e9cc2e5c8579db6fabb1594724eac733474b1f
GalaxyMan20 <88583795+A7109@users.noreply.github.com>
2023-02-25 21:13:38 +0000

    Create Mod.js

---
+++ Mod.js
@@ -0,0 +1,13 @@
+//this right here is a mod people
+
+
+elements.cocacola = {
+    color: "#ff0000",
+    behavior: behaviors.LIQUID,
+    category: "liquids",
+    viscosity: 100000,
+    state: "liquid",
+    density: 720,
+  
+  //i have no idea what this does but it does something
+};
